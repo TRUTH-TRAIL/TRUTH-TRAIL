8908f677125e2a1df09e59fcd1ae79cab688e111
newtron-vania <galtab0403@gmail.com>
2023-06-18 20:42:47 +0900

    CineMachin Camera Update

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    private float _deltaMouseX;
+    private float _deltaMouseY;
+    // Start is called before the first frame update
+    public void OnUpdate()
+    {
+        SetDeltaMousePos();
+    }
+
+    private void SetDeltaMousePos()
+    {
+        _deltaMouseX = Input.GetAxisRaw("Mouse X");
+        _deltaMouseX = Input.GetAxisRaw("Mouse Y");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
