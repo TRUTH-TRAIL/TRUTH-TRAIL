03f3a66f63099eaf7f88a2d104e2214a60469998
TSATOA <tg8591@gmail.com>
2023-07-21 22:06:15 +0900

    fix prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //FootGauge UI를 위한 임시 적용
 
 public class Player : MonoBehaviour
 {
+    //FootGauge UI 표시를 위한 임시 선언
+    [SerializeField]
+    private Slider footGaugeSlider;
+    [SerializeField]
+    private Text footText;
+
+    private float speed = 4f;
+
     //발소리 게이지
     private float footGauge;  //내부적으로 작용되는 발소리 게이지
     private int minFootGauge = 0;
     private int maxFootGauge = 100;
     private float footGaugeTimer; 
     private float footGaugeIncreaseInterval = 1f;
-
+    private float footGaugeDecreaseTimer = 2f;
     //특수용지
     bool activePaper = false;
     [SerializeField]
@@ -29,6 +38,10 @@ public class Player : MonoBehaviour
     {
         FootStepGauge();
         ViewPaper();
+
+        //FootGauge UI를 위한 임시 동작
+        footGaugeSlider.value = footGauge;
+        footText.text = footGauge.ToString("F1");
     }
 
     private void FootStepGauge() //발소리 게이지 관리
@@ -37,27 +50,41 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.LeftShift)) //달릴 때
             {
-                UpdateFootGauge(1.5f);
+                UpdateFootGauge(1.5f*speed);
             }
             else //걸을 때
             {
-                UpdateFootGauge(1.0f);
+                UpdateFootGauge(1.0f * speed);
             }
+            footGaugeDecreaseTimer = 2f;
         }
         else //가만히 있을 때
         {
-            UpdateFootGauge(-2.0f);
+            UpdateFootGauge(-2.0f * speed);
         }
     }
 
     private void UpdateFootGauge(float n) //발소리 게이지 조절
     {
-        footGaugeTimer -= Time.deltaTime;
-        if (footGaugeTimer <= 0)
+        if (n > 0)
         {
-            footGauge += n;
-            footGauge = Mathf.Clamp(footGauge, minFootGauge, maxFootGauge);
-            footGaugeTimer = footGaugeIncreaseInterval;
+            footGaugeTimer -= Time.deltaTime;
+            if (footGaugeTimer <= 0)
+            {
+                footGauge += n;
+                footGauge = Mathf.Clamp(footGauge, minFootGauge, maxFootGauge);
+                footGaugeTimer = footGaugeIncreaseInterval;
+            }
+        }
+        else
+        {
+            footGaugeDecreaseTimer -= Time.deltaTime;
+            if (footGaugeDecreaseTimer <= 0)
+            {
+                footGauge += n;
+                footGauge = Mathf.Clamp(footGauge, minFootGauge, maxFootGauge);
+                footGaugeDecreaseTimer = footGaugeIncreaseInterval;
+            }
         }
     }
 
@@ -75,4 +102,6 @@ public class Player : MonoBehaviour
             Time.timeScale = 1;
         }
     }
+
+
 }
