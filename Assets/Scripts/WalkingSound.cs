493a3864ef41baf59c5f5106db0b205c5133ccc3
Your Name <doto5004@naver.com>
2023-09-02 15:47:09 +0900

    속도/ BGM/ 발소리

---
+++ Assets/Scripts/WalkingSound.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WalkingSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public float playDelayTime = 2f;
+    private float delta = 0;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        delta += Time.deltaTime;
+        if(delta >= playDelayTime)
+        {
+            //Play();
+        }
+    }
+
+    void Play()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            audioSource.Play();
+        }
+    }
+}
+
