5c320f8952c97a997125e72e21bc5b9fe6e35ea2
young-46 <pink495sy@gmail.com>
2024-01-20 00:38:23 +0900

    파티클 모으기 + 앨리 수정_2차

--- Assets/Scripts/NewAlley.cs
+++ Assets/Scripts/NewAlley.cs
@@ -15,7 +15,7 @@ public class NewAlley : MonoBehaviour
        // LoadingScene.Instance.LoadScene("Death");
     }
     IEnumerator Time(){
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         Alley.SetActive(true);
         BGM.SetActive(true);
         transform.gameObject.SetActive(false);
